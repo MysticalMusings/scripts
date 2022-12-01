a287cab778c80aec1cb220d7e2bf956f5376a7b6
luminislight <39690782+luminislight@users.noreply.github.com>
2022-12-02 01:09:28 +0800

    Create termux-ubuntu.sh

---
+++ termux-ubuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+apt-get update && apt-get upgrade -y
+apt-get install wget -y
+apt-get install proot -y
+apt-get install git -y
+cd ~
+git clone https://github.com/MFDGaming/ubuntu-in-termux.git
+cd ubuntu-in-termux
+chmod +x ubuntu.sh
+./ubuntu.sh -y
+echo -e "\n\n输入./startubuntu.sh运行"
