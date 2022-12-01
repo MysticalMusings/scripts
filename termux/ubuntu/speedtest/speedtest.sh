2ffbaa764b3826361ee93621f607f4b6f68a0d02
luminislight <luminislight@outlook.com>
2022-12-02 05:42:44 +0800

    test

--- termux/ubuntu/speedtest/speedtest.sh
+++ termux/ubuntu/speedtest/speedtest.sh
@@ -7,9 +7,9 @@ cat * > ../all.txt
 cd ../OpenBullet2
 
 # 需要修改配置路径
-echo | dotnet OpenBullet2.Console.dll --bots 100 --config ../cf.opk -v -w ../cloudflare/all.txt --wltype Default | grep -o "[0-9].*$" > ../output.txt
+echo | dotnet OpenBullet2.Console.dll --bots 100 --config ../cf.opk -v -w ../all.txt --wltype Default | grep -oP "(?<=SUCCESS:\s)[0-9].*" > ../output.txt
 cd ..
 
 # 修改测速网址
-./CloudflareST -t 7 -dt 7 -f ip.txt -dn 10 -o best.csv -allip -t 7 -p 10 -url https://cloudflaremirrors.com/archlinux/images/latest/Arch-Linux-x86_64-basic.qcow2
+./CloudflareST -t 7 -dt 7 -f output.txt -dn 10 -o best.csv -allip -t 7 -p 10 -url https://cloudflaremirrors.com/archlinux/images/latest/Arch-Linux-x86_64-basic.qcow2
 rm output.txt
