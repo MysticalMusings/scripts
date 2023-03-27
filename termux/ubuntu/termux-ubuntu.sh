b70dff4b44271f95d9aa2ee7322679f4dc88eb41
MysticalMusings <39690782+MysticalMusings@users.noreply.github.com>
2023-03-27 14:46:53 +0800

    Update termux-ubuntu.sh

--- termux/ubuntu/termux-ubuntu.sh
+++ termux/ubuntu/termux-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
-apt-get update && apt-get upgrade
-apt-get install wget git proot
+apt-get update && apt-get upgrade -y
+apt-get install wget git proot -y
 cd ~
 git clone https://github.com/MFDGaming/ubuntu-in-termux.git
 cd ubuntu-in-termux
