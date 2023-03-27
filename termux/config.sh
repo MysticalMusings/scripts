bb54a76e612507e53df8dd8d691ed24b6ae6b4c0
MysticalMusings <39690782+MysticalMusings@users.noreply.github.com>
2023-03-27 15:04:08 +0800

    Update config.sh

--- termux/config.sh
+++ termux/config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh | bash
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
 sed -i '1 iexport PATH=$PATH:~/bin'  ~/.zshrc
