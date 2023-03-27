b928f8c7490d6487f971e778a84f2c17d8a00af8
MysticalMusings <39690782+MysticalMusings@users.noreply.github.com>
2023-03-27 15:52:59 +0800

    Update install.sh

--- termux/ubuntu/install.sh
+++ termux/ubuntu/install.sh
@@ -7,4 +7,4 @@ git clone https://github.com/MFDGaming/ubuntu-in-termux.git
 cd ubuntu-in-termux
 chmod +x ubuntu.sh
 ./ubuntu.sh -y
-echo  -e "\n\n输入./ubuntu-in-termux/startubuntu.sh运行"
+echo  -e "\n\n输入 ./ubuntu-in-termux/startubuntu.sh 运行"
