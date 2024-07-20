3f9129560869d1328b57ea431598be1e69874508
MysticalMusings <39690782+MysticalMusings@users.noreply.github.com>
2024-07-20 13:58:30 +0800

    更新 config.sh

--- termux/config.sh
+++ termux/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+#sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
 apt update && apt upgrade -y
 apt install openssh -y
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
